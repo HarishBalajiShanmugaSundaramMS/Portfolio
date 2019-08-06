345d5136efb01dee688f49c145a11d760d2a8bb9
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-06 22:51:50 +0200

    Text Level Formatting Done. Feedback Form Added.

--- Projects/Customised_Translator.py
+++ Projects/Customised_Translator.py
@@ -5,13 +5,13 @@ import re
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog, messagebox, ttk
-from tkcalendar import Calendar, DateEntry
-from PyQt5 import QtGui
 
 import pymysql
 from googletrans import Translator
 from PIL import Image, ImageTk
 from PyDictionary import PyDictionary
+from PyQt5 import QtGui
+from tkcalendar import Calendar, DateEntry
 
 #dictionary = PyDictionary("Dogs")
 #print (dictionary.translateTo("ta"))
@@ -52,20 +52,21 @@ tab1 = ttk.Frame(tab_parent)
 tab2 = ttk.Frame(tab_parent)
 tab3 = ttk.Frame(tab_parent)
 tab4 = ttk.Frame(tab_parent)
+tab5 = ttk.Frame(tab_parent)
+
 
 
 tab_parent.add(tab1, text='Translate')
 tab_parent.add(tab2, text='Reports')
 tab_parent.add(tab3, text='Preferences')
 tab_parent.add(tab4, text='Calendar')
+tab_parent.add(tab5, text='Feedback')
+
 
 
 show = Label(form, text='Using Translation to Learn a New Language')
-show.config(font=('bold'))
+show.config(font='Arial 23 bold', bg='#ececec')
 show.pack()
-show1 = Label(form, text='*****************************************')
-show1.config(font=('bold'))
-show1.pack()
 
 # Controls of First Tab
 
@@ -81,7 +82,7 @@ def callback():
         for translation in translations:
             total = str(e1.get())
             l.config(text='Translation = %s' % translation.text)
-            l.config(font=('bold'))
+            l.config(bg='#ececec', font='Arial 15 bold', fg='Green')
             # print(translations[0])
             print('English Text: ', translations[0].pronunciation)
             print('German Text: ', translations[0].text)
@@ -101,6 +102,7 @@ def calldelete():
 def applySettings():
     tex = str(e2.get())
     msg = messagebox.showwarning('Alert', tex)
+
 
 
 def closeWindow():
@@ -139,16 +141,17 @@ def previewCSV():
     root2.mainloop()
 
 
-textlab6 = Label(tab1, text='Select number of words:', bg='#ececec')
-textlab6.config(font=('bold'))
+textlab6 = Label(tab1, text='Select Number of Words:', bg='#ececec')
+textlab6.config(font='Arial 15 bold')
 textlab6.pack()
 
-w = Scale(tab1, from_=2, to=4, orient=HORIZONTAL, bg='#ececec')
-w.set(4)
+w = Scale(tab1, from_=2, to=5, orient=HORIZONTAL, bg='#ececec',
+          width=15, sliderlength=25, length=350, font='Arial 15 bold')
+w.set(5)
 w.pack()
 
 textlab1 = Label(tab1, text='Enter Your Text:', bg='#ececec')
-textlab1.config(font=('bold'))
+textlab1.config(font='Arial 15 bold')
 textlab1.pack()
 e1 = Entry(tab1)
 
@@ -167,18 +170,22 @@ e1.pack()
 #wordList = re.sub("[^\w]", " ",  mystr).split()
 # print(wordList)
 
-l = Label(tab1, bd=5, bg='#ececec')
+l = Label(tab1, bd=5)
+l.config(bg='#ececec', font='Arial 15 bold')
+l.pack()
+
+
 b1 = Button(tab1, text='Translate', command=callback, bd=5, width=10)
-b1.config(font=('bold'))
+b1.config(font='Arial 15 bold')
 b2 = Button(tab1, text='Clear', command=calldelete, width=10, bd=5)
-b2.config(font=('bold'))
+b2.config(font='Arial 15 bold')
 b4 = Button(tab1, text='Progress', command=previewCSV, width=10, bd=5)
-b4.config(font=('bold'))
+b4.config(font='Arial 15 bold')
 
 
 c1 = Checkbutton(tab1, text='Capture Datalogs', bg='#ececec',
                  offvalue='unchecked', onvalue='checked')
-c1.config(font=('bold'))
+c1.config(font='Arial 15 bold')
 c1.select()
 c1.pack()
 
@@ -188,8 +195,8 @@ for widget_tab1 in (e1, l, b1, b2, b4):
 
 # Controls of Second Tab
 
-textlab2 = Label(tab2, text='You can see your reports here:', bg='#ececec')
-textlab2.config(font=('bold'))
+textlab2 = Label(tab2, text='You Can See Your Reports Here:', bg='#ececec')
+textlab2.config(font='Arial 15 bold')
 textlab2.pack()
 
 MODES = [
@@ -209,8 +216,8 @@ for text, mode in MODES:
 
 # Controls of Third Tab
 
-textlab6 = Label(tab3, text='Select your Path:', bg='#ececec')
-textlab6.config(font=('bold'))
+textlab6 = Label(tab3, text='Select Your Path:', bg='#ececec')
+textlab6.config(font='Arial 15 bold')
 textlab6.pack()
 
 e2 = Entry(tab3)
@@ -218,32 +225,32 @@ e2.config(bg='light yellow', font=('bold'), bd=3, width=400)
 e2.pack()
 
 textlab3 = Label(
-    tab3, text='Select the langage you want to learn:', bg='#ececec')
-textlab3.config(font=('bold'))
+    tab3, text='Select the Language You Want to Learn:', bg='#ececec')
+textlab3.config(font='Arial 15 bold')
 textlab3.pack()
 
 OptionList1 = ["English", "German", "Tamil"]
 o_variable1 = StringVar(tab3)
 o_variable1.set(OptionList1[1])
 opt1 = OptionMenu(tab3, o_variable1, *OptionList1)
-opt1.config(width=5, font=('TkDefaultFont', 9, 'bold'))
+opt1.config(width=5, font='Arial 15 bold')
 opt1.pack()
 
 textlab4 = Label(
-    tab3, text='Select the langage you want to learn through:', bg='#ececec')
-textlab4.config(font=('bold'))
+    tab3, text='Select the Language You Want to Learn Through:', bg='#ececec')
+textlab4.config(font='Arial 15 bold')
 textlab4.pack()
 
 OptionList2 = ["English", "German", "Tamil"]
 o_variable2 = StringVar(tab3)
 o_variable2.set(OptionList2[0])
 opt2 = OptionMenu(tab3, o_variable2, *OptionList2)
-opt2.config(width=5, font=('TkDefaultFont', 9, 'bold'))
+opt2.config(width=5, font='Arial 15 bold')
 opt2.pack()
 
 
-textlab5 = Label(tab3, text='Select your native langage:', bg='#ececec')
-textlab5.config(font=('bold'))
+textlab5 = Label(tab3, text='Select Your Native Language:', bg='#ececec')
+textlab5.config(font='Arial 15 bold')
 textlab5.pack()
 
 
@@ -251,26 +258,23 @@ OptionList3 = ["English", "German", "Tamil"]
 o_variable3 = StringVar(tab3)
 o_variable3.set(OptionList3[2])
 opt3 = OptionMenu(tab3, o_variable3, *OptionList3)
-opt3.config(width=5, font=('TkDefaultFont', 9, 'bold'))
+opt3.config(width=5, font='Arial 15 bold')
 opt3.pack()
 
 
-b5 = Button(tab3, text='Apply', command=applySettings, width=10, bd=5)
-b5.config(font=('bold'))
+b5 = Button(tab3, text='Save Preferences', command=applySettings, width=15, bd=5)
+b5.config(font='Arial 15 bold')
 b5.pack()
+
+
 
 # Controls of Fourth Tab
 
-cal = Calendar(tab4, selectmode='day', background='#ececec', foreground='Black',
-                   tooltipforeground='Green', bordercolor='Black', selectbackground='Red', selectforeground='Green',
-                   weekendbackground='Red', weekendforeground='Red',
-                   othermonthforeground='gray', showothermonthdays=FALSE,
-                   headersforeground='Blue', borderwidth=2)
-date = cal.datetime.today() + cal.timedelta(days=2)
-cal.calevent_create(date, 'Hello World', 'message')
-cal.calevent_create(date, 'Reminder 2', 'reminder')
-cal.calevent_create(date + cal.timedelta(days=-2),
-                        'Reminder 1', 'reminder')
+cal = Calendar(tab4, selectmode='none', background='#ececec', foreground='Black',
+               tooltipforeground='Green', bordercolor='Black', selectbackground='Red', selectforeground='Green',
+               weekendbackground='Red', weekendforeground='Red',
+               othermonthforeground='Lightgray', showothermonthdays=True,
+               headersforeground='Blue', borderwidth=2, font='Arial 15 bold')
 
 cal.tag_config('reminder', highlightbackground='red', foreground='green')
 
@@ -281,7 +285,7 @@ tab_parent.pack(expand=1, fill='both')
 
 b3 = Button(form, text='Close', command=closeWindow,
             width=10, bd=5, fg='black', bg='lightblue')
-b3.config(font=('bold'))
+b3.config(font='Arial 15 bold')
 b3.pack()
 
 form.mainloop()
