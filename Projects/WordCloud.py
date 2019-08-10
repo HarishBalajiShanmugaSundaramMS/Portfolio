9bd4fb92f537264356a46e40f36f0796e21cb648
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-10 09:00:39 +0200

    Initial Commit

---
+++ Projects/WordCloud.py
@@ -0,0 +1,23 @@
+# Libraries
+import os
+import os.path
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+dir = r'/Users/harishbalaji/documents/mycode/mycode/mypythoncode/WordCloud'
+if not os.path.exists(dir):
+    os.mkdir(dir)
+
+
+# Create a list of word
+text = ('Harish Suresh Dakshayani Sundar Durga Renuka Devasena')
+wordcloud = WordCloud(width=1600, height=800).generate(text)
+plt.figure(figsize=(20, 10), facecolor='k')
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.margins(x=0, y=0)
+plt.tight_layout(pad=0)
+
+# Save the word cloud as an svg file 
+plt.savefig('/Users/harishbalaji/documents/mycode/mycode/mypythoncode/WordCloud/My_Word_Cloud.svg', format='svg', dpi=1200)
