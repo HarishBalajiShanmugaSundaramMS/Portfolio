a9fb6bc9a8bbcc165c5c45ae1d9840a2a208a994
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-16 18:16:56 +0200

    Removed Unwanted Libraries

--- Projects/Palindrome.py
+++ Projects/Palindrome.py
@@ -1,15 +1,9 @@
-from itertools import permutations
-import enchant
-import timeit
-
-d = enchant.Dict("en_US")
 word = input("Enter a Word: ")
-
-L=list(word)
+L = list(word)
 print(L)
-print(L[::-1]) # Reversed Copy of the List
+print(L[::-1])  # Reversed Copy of the List
 
-if (L== L[::-1]):
-        print('Palindrome')
+if (L == L[::-1]):
+    print('Palindrome')
 else:
-        print('Not a Palindrome')
+    print('Not a Palindrome')
