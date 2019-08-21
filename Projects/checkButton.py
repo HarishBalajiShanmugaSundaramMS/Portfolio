b142a796ddc3a7f36f20241cc03cda16927d0086
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-22 00:14:02 +0200

    CheckButtons RadioButtons Simple Code Added.

---
+++ Projects/checkButton.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import *
+form = tk.Tk()
+form.title('Tkinter Check Button')
+form.geometry('300x100')
+form.resizable(0, 0)
+chkValue01 = tk.StringVar()
+chkValue02 = tk.StringVar()
+c1 = Checkbutton(form, text='Checked By Default', var=chkValue01,
+                 offvalue='unchecked', onvalue='checked')
+c1.config(font='Arial 15 bold')
+c1.select()
+c1.pack()
+labelControl01 = Label(form, text=chkValue01.get(),
+                       font='Arial 15 bold', fg='green')
+labelControl01.pack()
+c2 = Checkbutton(form, text='Unchecked By Default',
+                 var=chkValue02, offvalue='unchecked', onvalue='checked')
+c2.config(font='Arial 15 bold')
+c2.deselect()
+c2.pack()
+labelControl02 = Label(form, text=chkValue02.get(),
+                       font='Arial 15 bold', fg='red')
+labelControl02.pack()
+form.mainloop()
+
+
---
+++ Projects/radioButton.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import *
+form = tk.Tk()
+form.title('Tkinter Radio Button')
+form.geometry('300x200')
+form.resizable(0, 0)
+def selectionCall():
+    selection = "The Selected Option is " + str(radValue.get())
+    label.config(text=selection, font='Arial 15 bold', fg='green')
+radValue = IntVar()
+r1 = tk.Radiobutton(form, text='Option 1', variable=radValue,
+                    value=1, command=selectionCall)
+r1.pack(side='top', anchor='w')
+r2 = tk.Radiobutton(form, text='Option 2', variable=radValue,
+                    value=2, command=selectionCall)
+r2.pack(side='top', anchor='w')
+r3 = tk.Radiobutton(form, text='Option 3', variable=radValue,
+                    value=3, command=selectionCall)
+r3.pack(side='top', anchor='w')
+label = Label(form)
+label.pack()
+form.mainloop()
+
+
+
