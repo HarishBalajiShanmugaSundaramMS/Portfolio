d0a195483f4ca180e6d0d210c20347c5a0e5d66d
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-22 19:14:13 +0200

    Tkinter ProgressBars Code Initial Commit

---
+++ Projects/progressBars01.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+form = tk.Tk()
+form.title('Tkinter ProgressBars')
+form.geometry('300x150')
+form.resizable(0, 0)
+label01 = Label(form)
+label01.config(text='Determinate ProgressBar', font='Arial 15 bold', fg='blue')
+label01.pack()
+progress01 = ttk.Progressbar(form, orient=HORIZONTAL, length=100, mode='determinate')
+progress01.pack()
+progress01.start()
+label02 = Label(form)
+label02.config(text='Indeterminate ProgressBar',font='Arial 15 bold', fg='red')
+label02.pack()
+progress02 = ttk.Progressbar(form, orient=HORIZONTAL, length=200, mode='indeterminate')
+progress02.pack()
+progress02.start()
+label03 = Label(form, text='Horizontal ProgressBars',font='Arial 23 bold', fg='Green')
+label03.pack()
+label04 = Label(form, text='Indeterminate ProgressBars Appear Static on Mac :-(',
+                font='Arial 8 bold', fg='Red')
+label04.pack()
+form.mainloop()
+
