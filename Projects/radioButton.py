72935cb2bd02a1e19de19e8d7d106ec40061e232
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-08-26 10:54:07 +0200

    Checked. Working Fine.

--- Projects/radioButton.py
+++ Projects/radioButton.py
@@ -1,12 +1,22 @@
+# ======================================
+# Title:  Tkinter RadioButtons
+# Author: HarishBalaji ShanmugaSundaram
+# Date:   09 August 2019
+# ======================================
 import tkinter as tk
 from tkinter import *
+
 form = tk.Tk()
 form.title('Tkinter Radio Button')
 form.geometry('300x200')
 form.resizable(0, 0)
+
+
 def selectionCall():
     selection = "The Selected Option is " + str(radValue.get())
     label.config(text=selection, font='Arial 15 bold', fg='green')
+
+
 radValue = IntVar()
 r1 = tk.Radiobutton(form, text='Option 1', variable=radValue,
                     value=1, command=selectionCall)
@@ -20,6 +30,3 @@ r3.pack(side='top', anchor='w')
 label = Label(form)
 label.pack()
 form.mainloop()
-
-
-
