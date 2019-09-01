bd27f96b4332ccfd020124becf9cf8e485591ec0
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-09-01 08:42:10 +0200

    Simple Swap.

---
+++ Projects/swap.py
@@ -0,0 +1,18 @@
+
+# TODO jsjf
+# ! Dont Use
+# * Important
+# ? Doubt
+
+a = 10
+b = 20
+
+print('Before Swap:')
+print('A= ', a)
+print('B= ', b)
+
+a, b = b, a
+
+print('After Swap:')
+print('A= ', a)
+print('B= ', b)
